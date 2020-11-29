774eee110ce6444e5725db8a08dcb03a25623bcb
kaushal_pareek <kaushal.pareek@preqin.com>
2020-11-29 13:45:13 +0530

    Substring of a rotated string

---
+++ PracticeApp/StringProblems/SubstringInRotatedString.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PracticeApp.StringProblems
+{
+    class SubstringInRotatedString
+    {
+        static bool isRotation(string a, string b)
+        {
+            string aa = a + a;
+            return aa.Contains(b);
+        }
+
+        static void Main1(string[] args)
+        {
+            string b = "waterbottle";
+            string a = "erbottlewat";
+            var res = isRotation(a, b);
+
+            Console.WriteLine(res);
+        }
+    }
+}
