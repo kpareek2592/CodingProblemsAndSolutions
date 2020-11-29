1914428bed1ea11d3816d5f11da262927a90ba39
kaushal_pareek <kaushal.pareek@preqin.com>
2020-11-29 11:56:52 +0530

    Checking if string has duplicate characters

--- PracticeApp/StringProblems/DuplicateCharacters.cs
+++ PracticeApp/StringProblems/DuplicateCharacters.cs
@@ -6,5 +6,28 @@ namespace PracticeApp.StringProblems
 {
     public class DuplicateCharacters
     {
+        static bool UniqueCharacters(string a)
+        {
+            int val;
+            a = a.Replace(" ", string.Empty);
+            bool[] char_set = new bool[256];
+            for (int i = 0; i < a.Length; i++)
+            {
+                val = a[i];
+                if (char_set[val])
+                    return false;
+                else
+                    char_set[val] = true;
+            }
+            return true;
+        }
+
+        static void Main1(string[] args)
+        {
+            string a = "apple is";
+            var res = UniqueCharacters(a);
+
+            Console.WriteLine(res);
+        }
     }
 }
