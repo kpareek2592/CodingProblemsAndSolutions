31cafc72f9999b133240849d937c95d4c6bf1051
kaushal_pareek <kaushal.pareek@preqin.com>
2020-11-29 13:44:39 +0530

    Anagram

---
+++ PracticeApp/StringProblems/Anagram.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PracticeApp.StringProblems
+{
+    // Check if one string is permutation of another string
+    class Anagram
+    {
+        static bool IsAnagram(string a, string b)
+        {
+            var dict = new Dictionary<char, int>();
+            if (a.Length != b.Length)
+                return false;
+            foreach (var ele in a)
+            {
+                if (!dict.ContainsKey(ele))
+                {
+                    dict[ele] = 1;
+                }
+                else
+                    dict[ele]++;
+            }
+
+            foreach (var ele in b)
+            {
+                if (!dict.ContainsKey(ele))
+                {
+                    dict[ele] = 1;
+                }
+                else
+                    dict[ele]++;
+            }
+
+            foreach (var ele in dict.Values)
+            {
+                if (ele % 2 != 0)
+                    return false;
+            }
+
+            return true;
+        }
+
+        static void Main1(string[] args)
+        {
+            string a = "appla";
+            string b = "pplae";
+            var res = IsAnagram(a, b);
+
+            Console.WriteLine(res);
+        }
+    }
+}
---
+++ PracticeApp/StringProblems/Anagram2.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PracticeApp.StringProblems
+{
+    class Anagram2
+    {
+        static bool IsAnagram(string a, string b)
+        {
+            var a1 = a.ToCharArray();
+            var b1 = b.ToCharArray();
+
+            System.Array.Sort(a1);
+            System.Array.Sort(b1);
+
+            a = string.Join("", a1);
+            b = string.Join("", b1);
+
+            if (a != b)
+                return false;
+
+            return true;
+        }
+
+        static void Main2(string[] args)
+        {
+            string a = "appla";
+            string b = "pplae";
+            var res = IsAnagram(a, b);
+
+            Console.WriteLine(res);
+        }
+    }
+}
